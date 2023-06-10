5f9ccdda731e72b7296cbf616b7d35e0449bf5cb
Saintshack <tpatel072022@gmail.com>
2023-06-10 12:57:54 -0500

    Some commit message of your choice goes here

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <React.Fragment>
       <Header>
+      <meta name="description" content="The Little Lemon Website"/>
+      <meta name="og:title" content="Little Lemon"/>
+      <meta name="og:description" content="The Little Lemon Website"/>
+      <meta name="og:image" content="Little Lemon Logo"/>
        <img src = "Photo.jpg"></img>
       </Header>
       <Nav>
