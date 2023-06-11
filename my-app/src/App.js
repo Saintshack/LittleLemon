fe4a821357727f397b3d3da74ac10b904e42545f
Saintshack <tpatel072022@gmail.com>
2023-06-11 09:36:11 -0500

    Little Lemon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,45 @@
+import AnotherPhoto from './AnotherPhoto.png'
+import Photo from './Photo.jpg';
+import React,{ReactHTML} from 'react';
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <Header>
+      <header>
       <meta name="description" content="The Little Lemon Website"/>
       <meta name="og:title" content="Little Lemon"/>
       <meta name="og:description" content="The Little Lemon Website"/>
       <meta name="og:image" content="Little Lemon Logo"/>
-       <img src = "Photo.jpg"></img>
-       <link rel = "stylesheet" href = "App.css"></link>
-      </Header>
-      <Nav>
-        <ul>
-          <li><a href = ""></a></li>
-          <li><a href = ""></a></li>
-          <li><a href = ""></a></li>
-          <li><a href = ""></a></li>
+      <link rel = "stylesheet" href = "App.css"></link>
+      <img src = {Photo} width = "50" height = "50" alt="Photo"></img>
+      </header>
+      <nav>
+        <ul class = "Links">
+          <li><a href = "Menu">Menu</a></li>
+          <li><a href = "">Reserve a Table</a></li>
+          <li><a href = "">Cart</a></li>
         </ul>
-      </Nav>
-      <Main> 
-
-      </Main>
-      <Footer>
+      <h1>Little Lemon</h1>
+      <p>Chicago</p>
+      <img src = {AnotherPhoto} width = "350" height = "200" alt="AnotherPhoto"></img>
+      </nav>
+      <main>
+        <article>
+        <section>
+        <h2>About Little Lemon</h2>
+        <p>
+        Based in Chicago, Illinois, Little Lemon is a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+        </p>
+        </section>
+        </article>
+      </main>
+      <footer>
         <caption>
           Copyright Little Lemon
         </caption>
-      </Footer>
+      </footer>
+     
     </React.Fragment>
   );
 }
